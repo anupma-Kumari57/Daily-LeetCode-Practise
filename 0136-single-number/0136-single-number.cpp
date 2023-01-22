d06ecc05942f742d21f93610ca3f762f004818e9
Anupma Kumari <80195380+anupma-Kumari57@users.noreply.github.com>
2023-01-22 23:56:38 +0530

    Time: 63 ms (11.81%), Space: 16.9 MB (87.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int answer=0;
-        for(int i=0;i<nums.size();i++){
-            answer=answer^nums[i]; 
-        }
+        for(auto a:nums)answer=answer^a; 
         return answer;
     }
 };
