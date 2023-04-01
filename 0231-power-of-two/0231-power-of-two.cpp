2e1bfc561d22819cfef9548e10121ce4a8e92228
Anupma Kumari <80195380+anupma-Kumari57@users.noreply.github.com>
2023-04-01 11:16:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (15.00%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
        
+        long ans=1;
         
         for(int i=0;i<=30;i++){
-            int ans=pow(2,i);
-        
+            
             if(ans==n){
                 return true;
             }
+            
+            ans=ans*2;
             }
         return false;
 
