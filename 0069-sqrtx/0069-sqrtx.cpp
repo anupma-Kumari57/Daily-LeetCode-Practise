90aa94bbb3e6d6f2e66e836b7713dabd37a97539
Anupma Kumari <80195380+anupma-Kumari57@users.noreply.github.com>
2023-05-10 11:39:50 +0530

    Time: 7 ms (32.38%), Space: 5.9 MB (92.75%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
-    int mySqrt(int x) {
+    
+    long long int binarySearch(int m){
+        int s=0, e=m;
+        long long int mid=s+(e-s)/2;
+        long long int ans=-1;
         
-        return sqrt(x);
+        while(s<=e){
+            long long int square=mid*mid;
+            if(square==m){
+                return mid;
+            }
+            else if(square<m){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        
+        return ans;
+    }
+    int mySqrt(int x) {
+        return binarySearch(x);
     }
 };
